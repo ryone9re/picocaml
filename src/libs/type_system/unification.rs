2ae7c853fe5d98b628be2f637d962acc4bf5b42d
ryone9re <ryoichi.kurimoto@gmail.com>
2025-03-11 21:22:48 +0900

    Fix error message

--- src/libs/type_system/unification.rs
+++ src/libs/type_system/unification.rs
@@ -1,12 +1,12 @@
 use anyhow::Result;
 use thiserror::Error;
 
-use crate::{syntax::ast::Expression, type_system::type_environment::TypeEnvironment};
+use crate::type_system::type_environment::TypeEnvironment;
 
 #[derive(Debug, Error)]
 enum UnificationError {
-    #[error("Unification impossible {0}")]
-    Impossible(Expression),
+    #[error("Unification impossible")]
+    Impossible,
 }
 
 pub fn unify(type_environment: TypeEnvironment) -> Result<TypeEnvironment> {
