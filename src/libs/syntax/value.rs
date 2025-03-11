c8dc2c1447fe4e0bf95f8fc3174464316f040cd4
ryone9re <ryoichi.kurimoto@gmail.com>
2025-03-11 17:58:13 +0900

    Remove impl Display for Value

--- src/libs/syntax/value.rs
+++ src/libs/syntax/value.rs
@@ -1,5 +1,3 @@
-use std::fmt::Display;
-
 use crate::{
     adapter::{RBool, RInteger, Symbol},
     structure::Structure,
@@ -26,26 +24,4 @@ pub enum Value {
         car: Box<Value>,
         cdr: Box<Value>,
     },
-}
-
-impl Display for Value {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Value::Integer(n) => write!(f, "{}", n),
-            Value::Bool(b) => write!(f, "{}", b),
-            Value::Closure {
-                structure: _structure,
-                parameter,
-                body: _body,
-            } => write!(f, "fun {} -> (...)", parameter),
-            Value::RecClosure {
-                structure: _structure,
-                call_name,
-                parameter,
-                body: _body,
-            } => write!(f, "rec {} = fun {} -> (...)", call_name, parameter),
-            Value::Nil => write!(f, "[]"),
-            Value::Cons { car, cdr } => write!(f, "{} :: {}", car, cdr),
-        }
-    }
 }
